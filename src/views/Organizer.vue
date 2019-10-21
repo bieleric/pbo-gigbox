e45efd39d10b7d6396a051db1652ccb9231198da
Paul Will <paul.willam@gmx.de>
2019-10-21 15:05:56 +0200

    Layout Organizer,Upload

--- src/views/Organizer.vue
+++ src/views/Organizer.vue
@@ -1,15 +1,45 @@
 <template>
   <div class="organizer">
     <h1>Organizer</h1>
+    <main>
+      <b-container >
+        <b-row class ="mb-5 " >
+          <b-col cols ="5" class = "dates header"> Date </b-col>
+          <b-col cols ="5" class = "files header"> Your sheets </b-col>
+        </b-row>
+     </b-container>
+  </main>
   </div>
 </template>
 
 <script>
 export default {
-  
 }
 </script>
 
 <style lang="scss">
+main {
+  padding-top: 5%; 
+  display: flex;
+  flex-direction: row;
+
+  .header {
+    background-color: burlywood;
+  }
+
+  .files {
+    height: 100%;
+    border: solid 3px grey;
+    border-radius: 10px;
+    margin: 0 auto 0 auto;
+  }
+
+  .dates {
+    height: 100%;
+    border: solid 3px grey;
+    border-radius: 10px;
+    margin: 0 auto 0 auto;
+  }
+}
 
 </style>
--- src/views/Yournotes.vue
+++ src/views/Yournotes.vue
@@ -1,15 +1,53 @@
 <template>
   <div class="yournotes">
     <h1>Your Notes</h1>
+    <main>
+      <b-container >
+        <b-row class ="mb-5" >
+          <b-col class ="mb-5" cols="2" >
+            <b-button pill id ="addBtn"> Add </b-button>
+            <b-row>
+              <b-col>
+                <b-button pill id ="deleteBtn"> Delete </b-button>
+              </b-col>
+            </b-row>
+          </b-col>
+          <b-col cols="8" id = "files"> Your sheets </b-col>
+        </b-row>
+     </b-container>
+  </main>
   </div>
 </template>
 
 <script>
 export default {
+  name: "Yournotes",
+  data (){
+    return {
+      files: []
+    };
+  }
     
 }
 </script>
 
 <style lang="scss">
+main {
+  padding-top: 5%; 
+  display: flex;
+  flex-direction: row;
+
+  #deleteBtn{
+    margin-top:50%;
+  }
+
+  #files {
+    height: 100%;
+    border: solid 3px grey;
+    border-radius: 10px;
+    margin: 0 auto 0 auto;
+  }
+
+}
 
 </style>
