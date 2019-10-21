64035194eccf38b94fd4c4d761289fe74c00ab71
bieleric <biele.eric@gmail.com>
2019-10-21 13:07:37 +0200

    datepicker hinzugefügt

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import Datetime from 'vue-datetime';
+// You need a specific loader for CSS files
+import 'vue-datetime/dist/vue-datetime.css';
+
+Vue.use(Datetime);
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
--- src/views/Today.vue
+++ src/views/Today.vue
@@ -1,14 +1,11 @@
 <template>
-
   <div class="today">
     <h1>Today</h1>
-    <main>
+    <main class="d-flex p-2 bd-highlight">
       <section id="notesheet">
-
       </section>
-
       <section id="organizer">
-
+        <datetime id="datepicker" v-model="date" title="Dein Autritt" placeholder="Choose a date..."></datetime>
       </section>
     </main>
   </div>
@@ -17,13 +14,22 @@
 <script>
 // @ is an alias to /src
 //import HelloWorld from "@/components/HelloWorld.vue";
+//import Datepicker from "vuejs-datepicker";
+
+import { Datetime } from 'vue-datetime';
 
 export default {
-  name: "today",
+  name: "Datepicker",
   components: {
-    //HelloWorld
+    datetime: Datetime
+  },
+  data() {
+    return {
+      date: ""
+    }
   }
-};
+}
+
 </script>
 
 <style lang="scss">
@@ -32,7 +38,7 @@ main {
   flex-direction: row;
 
   #notesheet {
-    height: 80px;
+    height: 30em;
     width: 45%;
     border: solid 3px grey;
     border-radius: 10px;
@@ -40,14 +46,14 @@ main {
   }
   
   #organizer {
-    height: 80px;
+    height: 30em;
     width: 45%;
     border: solid 3px grey;
     border-radius: 10px;
     margin: 0 auto 0 auto;
+
+
   }
 }
-
-
 
 </style>
