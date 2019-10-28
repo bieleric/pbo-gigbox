bba92a8dce71de9beb2766f7ded0884f389349a8
bieleric <biele.eric@gmail.com>
2019-10-28 14:18:13 +0100

    Drag and Drop implementiert und Font-Awesome eingebunden, Add und Delete-Button gestylt

--- src/views/Yournotes.vue
+++ src/views/Yournotes.vue
@@ -5,14 +5,21 @@
       <b-container >
         <b-row class ="mb-5" >
           <b-col class ="mb-5" cols="2" >
-            <b-button pill id ="addBtn"> Add </b-button>
+            <label for="addBtn" class="btn"><font-awesome-icon icon="plus" class="icon" /></label>
+            <input type="file" id ="addBtn" style="display: none;">
             <b-row>
               <b-col>
-                <b-button pill id ="deleteBtn"> Delete </b-button>
+                <draggable v-model="list2" group="people" @start="drag=true" @end="drag=false">
+                  <label id ="deleteBtn" class="btn"><font-awesome-icon icon="trash-alt" class="icon" /></label>
+                </draggable>
               </b-col>
             </b-row>
           </b-col>
-          <b-col cols="8" id = "files"> Your sheets </b-col>
+          <b-col cols="8" id = "files">
+            <draggable v-model="list2" group="people" @start="drag=true" @end="drag=false">
+              <div class="drag w-90 text-white rounded m-2" v-for="element in list2" :key="element.id">{{element.name}}</div>
+            </draggable>
+          </b-col>
         </b-row>
      </b-container>
   </main>
@@ -20,11 +27,23 @@
 </template>
 
 <script>
+import draggable from 'vuedraggable';
+
 export default {
   name: "Yournotes",
+
+  components: {
+    draggable,
+  },
+
   data (){
     return {
-      files: []
+      files: [],
+      list2: [
+        { name: "Song 1", id: 0 },
+        { name: "Song 2", id: 1 },
+        { name: "Song 3", id: 2 }
+      ],
     };
   }
     
@@ -42,12 +61,33 @@ main {
   }
 
   #files {
-    height: 100%;
+    height: 500px;
     border: solid 3px grey;
     border-radius: 10px;
     margin: 0 auto 0 auto;
   }
 
+  .btn {
+    background-color: #42b983;
+    color: white;
+    cursor: pointer;
+    width: 100%;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .icon {
+    font-size: 300%;
+  }
+
+  .drag {
+    cursor: move;
+    height: 40px;
+    vertical-align: middle;
+    background-color: #42b983;
+  }
 }
 
 </style>
