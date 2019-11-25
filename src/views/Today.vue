7ce736ed339188a0506205355273fdd162861ce2
bieleric <biele.eric@gmail.com>
2019-11-25 13:58:42 +0100

    pdf Anzeige funktioniert nicht

--- src/views/Today.vue
+++ src/views/Today.vue
@@ -3,57 +3,72 @@
     <h1>Today</h1>
     <main class="d-flex p-2 bd-highlight">
       <section id="notesheet" class="rounded">
-        <pdf :src="path" :page="1" style="width:100%; margin: 0 auto 0 auto;">
+
+        <pdf src="src/assets/AnotherBrickInTheWall.pdf" :page="1">
           <template slot="loading">
-            loading PDF...
+            loading content here...
           </template>
         </pdf>
+
       </section>
       <section id="organizer" class="rounded">
         <datetime id="datepicker" class="theme-orange bg-light w-100 rounded-top border"  v-model="date" title="Dein Autritt" placeholder="Choose a date..."></datetime> 
           <draggable v-model="list2" group="people" @start="drag=true" @end="drag=false">
-            <div id="songs" class="drag w-90 text-white rounded m-2" v-for="element in list2" :key="element.id" v-on:click="showPdf(element.id)">{{element.name}}</div>
+            <div id="songs" class="drag w-90 text-white rounded m-2" v-for="element in list2" :key="element.id" @click="showPdf(element.id)">
+            {{ element.name }}
+            </div>
           </draggable>
       </section>
-
     </main>
   </div>
 </template>
 
 <script>
-import { Datetime } from 'vue-datetime';
-import draggable from 'vuedraggable';
-import pdf from 'pdfvuer';
+import { Datetime } from "vue-datetime";
+import draggable from "vuedraggable";
+import pdf from "pdfvuer";
+//import * as storage from "../assets/storage.js";
 
 export default {
   name: "Datepicker",
   components: {
     datetime: Datetime,
     draggable,
-    pdf,
+    pdf
   },
+
   data() {
     return {
       date: "",
       list2: [
-        { name: "Another Brick In The Wall", id: 0, path: "AnotherBrickInTheWall.pdf" },
-        { name: "Song 2", id: 1, path: "Wasanderes" },
-        { name: "Song 3", id: 2 }
+        {
+          name: "Another Brick In The Wall",
+          id: 0,
+          path: "AnotherBrickInTheWall.pdf"
+        },
+        {
+          name: "Song 2",
+          id: 1,
+          path: "path/to/song2.pdf"
+        },
+        {
+          name: "Song 3",
+          id: 2,
+          path: "path/to/song3.pdf"
+        }
       ],
-      path: "AnotherBrickInTheWall.pdf",
+      path: "src/assets/AnotherBrickInTheWall.pdf"
     };
   },
 
   methods: {
     showPdf: function(key) {
+      let pfad = this.list2[key].path;
 
-      this.path = this.list2[key].path;
-
-      alert(this.path);
-    },
+      alert(pfad);
+    }
   }
-}
-
+};
 </script>
 
 <style lang="scss">
@@ -101,8 +116,6 @@ main {
     border-bottom: solid 3px grey;
   }
 
-
-
   .drag {
     cursor: move;
     height: 40px;
@@ -110,5 +123,4 @@ main {
     background-color: #42b983;
   }
 }
-
 </style>
